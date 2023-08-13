9ef3e0c9be61618077441d0d3289b861810e862e
diegodag <dgaleano201611@gmail.com>
2023-08-13 07:33:29 -0500

    contact section complete

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 ((d) => {
-  const $form = d.querySelector(".contact-form"),
-    $loader = d.querySelector(".contact-form-loader"),
-    $response = d.querySelector(".contact-form-response");
+  const $btnMenu = d.querySelector(".menu-btn"),
+  $menu = d.querySelector(".menu");
 
-  $form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/diegodag.24@gmail.com", {
-      method: "POST",
-      body: new FormData(e.target),
-    })
-      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-      .then((json) => {
-        console.log(json);
-        $loader.classList.add("none"); // Corregido: Debe ser $loader.classList.add("none");
-        location.hash = "#gracias";
-        $form.reset();
-      })
-      .catch((err) => {
-        console.log(err);
-        let message =
-          err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
-        $response.querySelector(
-          "h3"
-        ).innerHTML = `Error ${err.status}: ${message}`;
-      })
-      .finally(() => {
-        $loader.classList.add("none");
-        setTimeout(() => {
-          location.hash = "#close";
-        }, 3000);
-      });
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
   });
-})(document);
+
+  d.addEventListener("click", (e) => {
+    if(!e.target.matches(".menu a")) return false;
+    
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+
+
+  //-------CONTAC-----------------------------------
+  const $form = d.querySelector(".contact-form"),
+  $loader = d.querySelector(".contact-form-loader"),
+  $response = d.querySelector(".contact-form-response");
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  $loader.classList.remove("none");
+  fetch("https://formsubmit.co/ajax/diegodag.24@gmail.com", {
+    method: "POST",
+    body: new FormData(e.target),
+  })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      $loader.classList.add("none"); // Corregido: Debe ser $loader.classList.add("none");
+      location.hash = "#gracias";
+      $form.reset();
+    })
+    .catch((err) => {
+      console.log(err);
+      let message =
+        err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+      $response.querySelector(
+        "h3"
+      ).innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      $loader.classList.add("none");
+      setTimeout(() => {
+        location.hash = "#close";
+      }, 3000);
+    });
+});
+})(document)
